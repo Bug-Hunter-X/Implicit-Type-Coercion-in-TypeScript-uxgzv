ec7f463052fa0e801ff188f774ebec7fe4edc0a3
BugHunter-X <askzenai@gmail.com>
2024-12-13 06:30:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,15 @@
+function printNumber(num: number): void {
+  console.log(num);
+}
+
+function printString(str: string): void {
+  console.log(str);
+}
+
+function printBoolean(bool: boolean): void {
+  console.log(bool);
+}
+
+printNumber(5);
+printString("hello");
+printBoolean(true); 
